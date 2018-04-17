693388e8e6f5c775094e0a84b838ab7243c8d629
congthanhptnk <congthanhptnk@gmail.com>
2018-04-17 13:24:24 +0300

    another try

--- src/java/Model/GlobalModel.java
+++ src/java/Model/GlobalModel.java
@@ -29,11 +29,55 @@ public class GlobalModel {
         private static final GlobalModel INSTANCE = new GlobalModel();
     }
     
+    //MARK: Image
     public List<Image> getAllImages(){
         return this.images;
     }
     
     public void addImage(Image image){
         this.images.add(image);
+    }
+    
+    public List<Image> getImagesOfCategory(String catName){
+        List<Image> imgCat = new ArrayList();
+        for(Image img: this.images){
+            if(img.getCatName().equals(catName)){
+                imgCat.add(img);
+            }
+        }
+        return imgCat;
+    }
+    
+    //MARK: Category
+    public List<Category> getAllCategories(){
+        return this.categories;
+    }
+    
+    public Category getSingleCategory(String catName){
+        Category singleCat = new Category();
+        for(Category cat: this.categories){
+            if(cat.getCatName().equals(catName)){
+                singleCat = cat;
+            }
+        }
+        return singleCat;
+    }
+    
+    //MARK: Item 
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+    
+    public String getCatNameFromItem(String shape, String material){
+        String catName = "";
+        for (Item combination : this.items) {
+            if (combination.getShape().equals(shape) && combination.getMaterial().equals(material)) {
+                catName = combination.getCatName();
+            }
+        }
+        if(catName.isEmpty() || catName == null){
+            catName = "other";
+        }
+        return catName;
     }
 }
